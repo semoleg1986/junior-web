585f149465f772891c7872ab81d27da642e5cddf
Oleg Semenov <66306538+semoleg1986@users.noreply.github.com>
2023-01-10 11:53:37 +0300

    Update models.py

--- templ/models.py
+++ templ/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Template(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название шаблона')
+    fields = models.ManyToManyField(Field)
+    def __str__(self):
+        return self.form_name
+class Field(models.Model):
+    CHOICE_TYPE = (
+        (text, 'text'),
+        (email, 'example@email.mail'),
+        (phone, '+7123456789'),
+        (date, '2023-01-01')
+    )
+
+    f_name = models.CharField(max_length=50)
+    f_type = models.CharField(max_length=5,
+                                   choices=CHOICE_TYPE,
+                                   default=text)
+    def __str__(self):
+        return self.f_тфьу
